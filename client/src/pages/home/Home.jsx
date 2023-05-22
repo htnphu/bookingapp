4c00658288a8f50056af8a1e7417776069e0b6c1
Han Phu <phuhanld@gmail.com>
2023-05-23 02:23:42 +0700

    Featured Properties UI component

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -4,6 +4,7 @@ import Featured from "../../components/featured/Featured";
 
 import "../../styles/home.css";
 import PropertyList from "../../components/propertyList/PropertyList";
+import FeaturedProperties from "../../components/featuredProperties/FeaturedProperties";
 
 const Home = () => {
   return (
@@ -14,6 +15,8 @@ const Home = () => {
         <Featured />
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList />
+        <h1 className="homeTitle">Homes guests love</h1>
+        <FeaturedProperties />
       </div>
     </div>
   );
